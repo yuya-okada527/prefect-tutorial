e834c5a68f5593eefed90504719269df29c85bac
yuya-okada527 <yuya.okada527@gmail.com>
2021-04-11 23:26:40 +0900

    update assert

--- src/app/main.py
+++ src/app/main.py
@@ -16,3 +16,13 @@ with Flow("My first flow!") as flow:
     second_result = add(x=first_result, y=100)
 
 state = flow.run()
+
+assert state.is_successful()
+
+first_task_state = state.result[first_result]
+assert first_task_state.is_successful()
+assert first_task_state.result == 3
+
+second_task_state = state.result[second_result]
+assert second_task_state.is_successful()
+assert second_task_state.result == 103
